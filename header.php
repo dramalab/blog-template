f575e47bf767685c1c0bbd6b23ba00d6c68e7add
Matteo Moretti <hello@matteomoretti.com>
2014-03-07 10:48:14 +0100

    testing gradient titles

---
+++ header.php
@@ -0,0 +1,82 @@
+<!doctype html>  
+
+<!--[if IEMobile 7 ]> <html <?php language_attributes(); ?>class="no-js iem7"> <![endif]-->
+<!--[if lt IE 7 ]> <html <?php language_attributes(); ?> class="no-js ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html <?php language_attributes(); ?> class="no-js ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html <?php language_attributes(); ?> class="no-js ie8"> <![endif]-->
+<!--[if (gte IE 9)|(gt IEMobile 7)|!(IEMobile)|!(IE)]><!--><html <?php language_attributes(); ?> class="no-js"><!--<![endif]-->
+	
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<title><?php wp_title( '|', true, 'right' ); ?></title>	
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				
+		<!-- media-queries.js (fallback) -->
+		<!--[if lt IE 9]>
+			<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>			
+		<![endif]-->
+
+		<!-- html5.js -->
+		<!--[if lt IE 9]>
+			<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+		<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo('template_url'); ?>/custom.css" />
+  		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+  		<script type="text/javascript" src="<?php echo get_template_directory_uri() ?>/library/js/jquery-1.11.0.min.js"></script>
+  		<script type="text/javascript" src="<?php echo get_template_directory_uri() ?>/library/js/jquery.gradienttext-0.1.min.js"></script>
+		
+		
+
+		<!-- wordpress head functions -->
+		<?php wp_head(); ?>
+		<!-- end of wordpress head -->
+			
+		<script>
+		$(document).ready(function(){
+		    $('h1').gradienttext({
+		        colors: [
+		            '#fd9135',
+		            '#f92739'
+		        ],
+        style: 'horizontal'
+    });
+});
+		</script>	
+	</head>
+	
+	<body <?php body_class(); ?>>
+				
+		<header role="banner">
+				
+			<div class="navbar navbar-default navbar-fixed-top">
+				<div class="container">
+          
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+
+						<a class="navbar-brand" title="<?php echo get_bloginfo('description'); ?>" href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a>
+					</div>
+
+					<div class="collapse navbar-collapse navbar-responsive-collapse">
+						<?php wp_bootstrap_main_nav(); // Adjust using Menus in Wordpress Admin ?>
+
+						<?php //if(of_get_option('search_bar', '1')) {?>
+						<form class="navbar-form navbar-right" role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
+							<div class="form-group">
+								<input name="s" id="s" type="text" class="search-query form-control" autocomplete="off" placeholder="<?php _e('Search','wpbootstrap'); ?>" data-provide="typeahead" data-items="4" data-source='<?php echo $typeahead_data; ?>'>
+							</div>
+						</form>
+						<?php //} ?>
+					</div>
+
+				</div> <!-- end .container -->
+			</div> <!-- end .navbar -->
+		
+		</header> <!-- end header -->
+		
+		<div class="container">
